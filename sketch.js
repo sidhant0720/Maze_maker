220c132c0db31dc1aa3ae407199f4a71c432d555
sidhant0720 <sidhant.choudhary434@gmail.com>
2020-07-12 08:25:02 +0530

    maze gen v1 Ct

--- sketch.js
+++ sketch.js
@@ -1,7 +1,168 @@
+var cols, rows;
+var w = 20;
+var grid = [];
+var current;
+var stack = [];
+
 function setup() {
   createCanvas(400, 400);
+  frameRate(20);
+  cols = floor(width / w);
+  rows = floor(height / w);
+
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
+    }
+  }
+
+  current = grid[0];
+
+
 }
 
 function draw() {
-  background(20,200,200);
+  background(51);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+
+  current.visited = true;
+  current.highlight();
+  //showStack(stack);
+
+  var next = current.checkNeighbours();
+  
+  if (next) {
+
+    next.visited = true;
+    stack.push(current);
+    removeWalls(current, next);
+    current = next;
+  }
+  else if(next==undefined || stack.length>0){
+    current=stack.pop();
+  }
 }
+
+function removeWalls(a, b) {
+
+  var x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  }
+  else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  var y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  }
+  else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+};
+
+function index(i, j) {
+  if (i < 0 || j < 0 || j > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+
+  return i + (j * cols);
+}
+
+
+function Cell(i, j) {
+  this.i = i;
+  this.j = j;
+  this.walls = [true, true, true, true];
+  this.visited = false;
+  this.inStack = false;
+
+  this.highlight = function () {
+    var x = this.i * w;
+    var y = this.j * w;
+    noStroke();
+    fill(200, 200, 0, 100);
+    rect(x, y, w, w);
+  }
+
+
+  this.checkNeighbours = function () {
+    var neighbours = [];
+    var top = grid[index(i, j - 1)];
+    var right = grid[index(i + 1, j)];
+    var bottom = grid[index(i, j + 1)];
+    var left = grid[index(i - 1, j)];
+
+    if (top && !top.visited) {
+      neighbours.push(top);
+    }
+    if (right && !right.visited) {
+      neighbours.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbours.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbours.push(left);
+    }
+
+    if (neighbours.length > 0) {
+      var r = floor(random(0, neighbours.length));
+      return neighbours[r]
+    }
+    else {
+      return undefined;
+    }
+  }
+
+
+
+  this.show = function () {
+    var x = this.i * w;
+    var y = this.j * w;
+
+    stroke(255);
+
+
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
+    if (this.visited) {
+      noStroke();
+      fill(200, 0, 0, 150);
+      rect(x, y, w, w);
+    }
+
+  }
+}
+
+// showStack = function(stack){
+
+//   for(var k=0;i<stack.length;k++){
+//     var x=stack[k].i*w;
+//     var y=stack[k].j*w;
+//     noStroke();
+//     fill(0, 0, 200, 50);
+//     rect(x, y, w, w);
+//     console.log();
+
+//   }
+
+
+// }
